5b20ee6909d23b0b0b4caefc900fb88ceccb534e
yokota <wb55yokota@gmail.com>
2015-03-01 13:48:00 +0900

    Initial Commit

---
+++ WBMediaControllerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WBMediaControllerApp
+//
+//  Created by YOKOTA, Kenji on 2015/03/01.
+//  Copyright (c) 2015年 YOKOTA, Kenji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
